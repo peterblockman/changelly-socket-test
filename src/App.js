894d39d9b3eae2bf689cb8259c281c8f0749b69d
vietvu-nguyen <vietvu.hcm@gmail.com>
2018-10-22 19:51:57 +0900

    1st deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { status } from './api';
 
+const initialState = {
+  status: 'no status'
+  };
 class App extends Component {
+  
+  constructor(props) {
+  super(props);
+  this.state = initialState
+  }
+  componentDidMount(){
+    status((data) => this.setState({status: data}))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <p className="tc">
+        Changelly socket: {this.state.status}
+      </p>
+    </div>
     );
   }
 }
---
+++ src/api.js
@@ -0,0 +1,50 @@
+import io from 'socket.io-client';
+// import cryptojs from 'crypto-js'
+var crypto = require('crypto')
+
+var URL = 'https://api.changelly.com';
+const apiKey ='myApiKey'
+const apiSecret ='myApiSecret'
+
+const sign = (message) => {
+      return crypto
+        .createHmac('sha512', apiSecret)
+        .update(JSON.stringify(message))
+        .digest('hex');
+    };
+
+var socket = io.connect(URL, {
+      'reconnection': true,
+      'reconnectionDelay': 1000,
+      'reconnectionDelayMax': 5000,
+      'reconnectionAttempts': 'Infinity'
+    });
+
+export const status = (cb) => {
+	socket.on('connect', () => {
+      var message = {
+        "Login": {}
+      };
+      socket.emit('subscribe', 
+        {
+          apiKey: apiKey,
+          sign: sign(message),
+          message: message
+        }
+      );
+    });
+   socket.on('status', (err, data) => {
+		console.log(data)
+	});
+    socket.on('payin', (err, data) => {
+	  cb(data)
+	  if(err){
+	  	console.log(err)
+	  }
+	});
+  }
+
+
+
+
+
